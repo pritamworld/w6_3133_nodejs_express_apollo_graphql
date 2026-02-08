4f4408702ff7f3e69db5daf9bf981a6a01fb3bbc
moxDroid Labs Inc <pritam.world@gmail.com>
2026-02-08 16:59:11 -0500

    Winter 2026 solution

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# References
+
+https://www.apollographql.com/docs/apollo-server/getting-started
+
 https://medium.com/better-programming/a-simple-crud-app-using-graphql-nodejs-mongodb-78319908f563
 
 https://medium.com/the-node-js-collection/making-your-node-js-work-everywhere-with-environment-variables-2da8cdf6e786
---
+++ schemas/schema.js
@@ -0,0 +1,25 @@
+// import { gql } from '@apollo/server';
+import { gql } from 'graphql-tag';
+
+const movieSchema = gql`
+    type Movie {
+        id: ID!
+        name: String!
+        director_name: String!
+        production_house: String!
+        release_date: String!
+        rating: Float!
+    },
+    type Query {
+        movies: [Movie]
+        movie(id: ID!): Movie
+        moviesByDirector(director_name: String!): [Movie]
+    },
+    type Mutation {
+        createMovie(name: String!, director_name: String!, production_house: String!, release_date: String!, rating: Float!): Movie
+        updateMovie(id: ID!, name: String, director_name: String, production_house: String, release_date: String, rating: Float): Movie
+        deleteMovie(id: ID!): Movie
+    }
+`;
+
+export default movieSchema;
